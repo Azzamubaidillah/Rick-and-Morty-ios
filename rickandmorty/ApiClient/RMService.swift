9a2ed04a578bac0c40aaac29dc060a6ecca6ca97
Azzam Ubaidillah <azzam.ubaidillah05@gmail.com>
2023-06-11 23:57:38 +0700

    api request handler

--- rickandmorty/ApiClient/RMService.swift
+++ rickandmorty/ApiClient/RMService.swift
@@ -18,9 +18,14 @@ final class RMService {
     /// send rick and morty api call
     /// - Parameters:
     ///     - Request: Request instance
+    ///     - type: The type of object we expect to get back
     ///     - completion: Callback with data or error
-    public func execute(_ request: RMRequest, completion: @escaping () -> Void){
-        
+    public func execute<T: Codable>(
+        _ request: RMRequest,
+        expecting type: T.Type,
+        completion: @escaping (Result<T, Error>) -> Void
+    ) {
+            
     }
     
 }
